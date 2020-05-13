0b3cdc23270a4b473ef3e6d8ea9dabb937f900e9
Oladiman <astonishingdami9@gmail.com>
2020-05-13 14:57:47 +0100

    Updates

---
+++ precache-manifest.706b71e58f80e78d476e3fae0f33f5a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7048c34852c35aa5b6e5489f3fe2dd",
+    "url": "/TodoListApp-react/index.html"
+  },
+  {
+    "revision": "49b66d07ea49a2988a0f",
+    "url": "/TodoListApp-react/static/css/main.42e531c6.chunk.css"
+  },
+  {
+    "revision": "80caa9fbcfa27cbbc321",
+    "url": "/TodoListApp-react/static/js/2.d950c583.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoListApp-react/static/js/2.d950c583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b66d07ea49a2988a0f",
+    "url": "/TodoListApp-react/static/js/main.2d9b16d3.chunk.js"
+  },
+  {
+    "revision": "2c8674e9d8a8f20e13a2",
+    "url": "/TodoListApp-react/static/js/runtime-main.da1aff5a.js"
+  }
+]);
